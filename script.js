1c54995f955c2dfc8d11e47811e656d8cc051167
BIKESHOPROMEO <tama@realeyes.co.jp>
2025-08-14 15:59:04 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -21,6 +21,7 @@
 
   // 送信ボタンのクリック処理
   document.getElementById("submitBtn").addEventListener("click", function () {
+	console.log("送信ボタンがクリックされました"); 
     this.disabled = true;
     document.getElementById("sendingDialog").style.display = "block";
 
@@ -34,6 +35,8 @@
 
     data.append("action", "create");
     data.append("selectedDateTime", `${selectedDate} ${selectedTime}`);
+	
+	console.log("fetch開始"); 
 
     fetch("https://script.google.com/macros/s/AKfycbwspV-0AlfTcuB3PlTMKmzot-a0b9h0Kv3zRyMpmJZWvRWorbfaNBVxNwvXm2DXMasO/exec", {
       method: "POST",
@@ -41,11 +44,17 @@
     })
       .then(res => res.text())
       .then(text => {
+
+	console.log("fetch成功:", text); 
+
         document.getElementById("sendingDialog").style.display = "none";
         alert(text);
         // 必要なら画面遷移などもここで
       })
       .catch(err => {
+
+	console.error("fetchエラー:", err);
+
         document.getElementById("sendingDialog").style.display = "none";
         this.disabled = false;
         alert("エラーが発生しました：" + err.message);
