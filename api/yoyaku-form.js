f1ad796a3b13419c2be2a20caa65e80b9196e018
BIKESHOPROMEO <tama@realeyes.co.jp>
2025-08-18 18:56:55 +0900

    Add files via upload

--- api/yoyaku-form.js
+++ api/yoyaku-form.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ message: "Method Not Allowed" });
   }
 
-  const GAS_URL = "https://script.google.com/macros/s/AKfycb.../exec"; // ←あなたのGAS URL
+  const GAS_URL = "https://script.google.com/macros/s/AKfycbwspV-0AlfTcuB3PlTMKmzot-a0b9h0Kv3zRyMpmJZWvRWorbfaNBVxNwvXm2DXMasO/exec"; // ←あなたのGAS URL
 
   try {
     const gasRes = await fetch(GAS_URL, {
